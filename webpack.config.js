9034055b945efa1ce6a7923b3b6365c2b1c06cc6
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-12-30 03:06:11 +0900

    基本的なことを書ける状態にした

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.scss/,
                 use: [
                     // linkタグに出力するローダ
                     "style-loader",
