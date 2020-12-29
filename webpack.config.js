75a306c6ffc718db647134c522709393fe1e7453
Eiichi Takebuchi <nanashi4129@gmail.com>
2020-12-30 00:54:15 +0900

    SASSを構成する

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,31 @@ module.exports = {
     // production -- 最適化
     mode: "development",
 
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { 
+                            url: false ,
+                            sourceMap: enabledSourceMap,
+                            importLoaders: 2
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: enabledSourceMap
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
     output: {
         path: `${__dirname}/dist`,
         filename: "index.js"
@@ -13,5 +38,7 @@ module.exports = {
     devServer: {
         contentBase: "dist",
         open: true
-    }
+    },
+
+    target: ["web", "es5"]
 };
